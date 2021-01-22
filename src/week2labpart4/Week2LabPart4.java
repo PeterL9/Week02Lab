a0a755c31ad97a1f3b2978acf175f01c8a7ef821
799768 <799768@SAIT225783IT.ACDM.DS.SAIT.CA>
2021-01-22 00:13:43 -0700

    Lab 2 Part 4 Random Number Generator prints out a random number

---
+++ src/week2labpart4/Week2LabPart4.java
@@ -0,0 +1,16 @@
+package week2labpart4;
+
+/**
+ * Random Number Generator for Week 2 Lab Part 4
+ * @author 799768
+ */
+public class Week2LabPart4 {
+
+    public static void main(String[] args) {
+        
+        // Create random number and print it out
+        double number = Math.random() * 100;
+        System.out.println("Your random Number is: " + number);
+    }
+    
+}
